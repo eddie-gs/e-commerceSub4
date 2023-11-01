c8010432ad2361c6233ab825537a2152a0dbac96
Sart1715 <santiarticardi@gmail.com>
2023-11-01 18:25:03 -0300

    agregando formulario de perfil

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,1 +1,6 @@
 document.getElementById("sesion").addEventListener("click", () => logout());
+
+document.addEventListener("DOMContentLoaded", () => {
+    let emailUsuario = sessionStorage.getItem("usuario") //toma el email del usuario ingresado 
+    document.getElementById("email").value = emailUsuario //le asigna al input el email del usario como valor 
+})
