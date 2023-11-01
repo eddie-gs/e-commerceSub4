dd6709dbf97e02948d3a3f939a4bbb3e83b0889a
Javier Gularte <133526577+eddie-gs@users.noreply.github.com>
2023-11-01 19:31:06 -0300

    myprofile.js

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,48 +1,58 @@
 document.getElementById("sesion").addEventListener("click", () => logout());
 
-//Traemos los valores de los campos del formulario de perfil
-const nombre1 = getElementById("nombre1").value;
-const nombre2 = getElementById("nombre2").value;
-const apellido1 = getElementById("apellido1").value;
-const apellido2 = getElementById("apellido2").value;
-const numero = getElementById("numero").value;
+const datosGuardados = JSON.parse(localStorage.getItem("perfil")) || {};
+let datosUsuario = {};
 
-//Creamos un objeto que contenga los datos del usuario
-const datosUsuario = {
-    nombre1 : nombre1,
-    nombre2 : nombre2,
-    apellido1 : apellido1,
-    apellido2 : apellido2,
-    email : sessionStorage.getItem("usuario"),
-    numero : numero,
-    imagenPerfil : localStorage.getItem("imagenPerfil")
-};
+document.addEventListener("DOMContentLoaded", () => {
+  let emailUsuario = sessionStorage.getItem("usuario"); //toma el email del usuario ingresado
+  document.getElementById("email").value = emailUsuario; //le asigna al input el email del usario como valor
+  console.log(datosGuardados);
 
-//Almacenar datos en localstorage del perfil
-function guardarCambios(datos) {
-    const datosGuardados = JSON.parse(localStorage.getItem("perfil")) || [];
-
+  //Almacenar datos en localstorage del perfil
+  function guardarCambios(datos) {
+    const datosGuardados = JSON.parse(localStorage.getItem("perfil")) || {};
     datosGuardados.push(datos);
+    console.log(datos);
+    localStorage.setItem("perfil", JSON.stringify(datos));
+  }
 
-    localStorage.setItem("perfil", JSON.stringify(datosGuardados));
-}
+  //Validacion de los campos del perfil
+  (function () {
+    "use strict";
 
-//Validacion de los campos del perfil
-(function () {
-    'use strict'
-  
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
+    var forms = document.querySelectorAll(".needs-validation");
+
+    Array.prototype.slice.call(forms).forEach(function (form) {
+      form.addEventListener(
+        "submit",
+        function (event) {
           if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
+            event.preventDefault();
+            event.stopPropagation();
           }
-          
-          guardarCambios(datosUsuario)
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+          //Traemos los valores de los campos del formulario de perfil
+          const nombre1 = document.getElementById("nombre1").value;
+          const nombre2 = document.getElementById("nombre2").value;
+          const apellido1 = document.getElementById("apellido1").value;
+          const apellido2 = document.getElementById("apellido2").value;
+          const numero = document.getElementById("numero").value;
+
+          //Creamos un objeto que contenga los datos del usuario
+          datosUsuario = {
+            nombre1: nombre1,
+            nombre2: nombre2,
+            apellido1: apellido1,
+            apellido2: apellido2,
+            email: sessionStorage.getItem("usuario"),
+            numero: numero,
+            imagenPerfil: localStorage.getItem("imagenPerfil"),
+          };
+          guardarCambios(datosUsuario);
+
+          form.classList.add("was-validated");
+        },
+        false
+      );
+    });
+  })();
+});
