205a62b0ebf904c2bfe44116e45fce1738197cc9
Sofía Hernández <sofiachp95@gmail.com>
2023-11-02 12:56:30 -0300

    Imagen del usuario guardad en el perfil

--- js/my-profile.js
+++ js/my-profile.js
@@ -8,9 +8,12 @@ function guardarCambios(datos) {
   console.log(datos);
   localStorage.setItem("perfil", JSON.stringify(datos));
 }
+ const imagenPerfilInput = document.getElementById("profileImg");
+ const imagenDePerfil = document.getElementById("imagen-perfil");
 
 document.addEventListener("DOMContentLoaded", () => {
   let emailUsuario = sessionStorage.getItem("usuario"); //toma el email del usuario ingresado
+
   if(datosGuardados != {} && datosGuardados.email != emailUsuario){ //vacia el objeto del localstorage si el mail distinto al que esta logueado
     datosGuardados = {}
     localStorage.setItem("perfil", JSON.stringify(datosGuardados));
@@ -20,11 +23,27 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("apellido1").value = datosGuardados.apellido1;
     document.getElementById("apellido2").value = datosGuardados.apellido2;
     document.getElementById("numero").value = datosGuardados.numero;
+    imagenDePerfil.setAttribute('src', datosGuardados.imagenPerfil);
+
+
   }
   document.getElementById("email").value = emailUsuario; //le asigna al input el email del usario como valor
 
   console.log(datosGuardados);
+
+  imagenPerfilInput.addEventListener("change", (event)=>{ //Utilizamos el evento 'change' que detecta si ha habido algún cambio en el input file donde el usuario cargará la imagen.
+    event.preventDefault(); //Evitamos que tome la foto de perfil por defecto
+    const archivo = imagenPerfilInput.files[0]; // Guardo en una variable los dato de la imagen cargada
+    const lector = new FileReader(); //Guardo en una variable el objeto que leerá la infromación del archivo cargado
+  
+        lector.addEventListener('load', () => { //Utilizamos el evento 'load' para que el objeto 'lector' obtenga los datos del archivo una vez se haya cargado exitosamente.
+            imagenDePerfil.setAttribute('src', lector.result); //Cambia el atributo src predeterminado con la info de la nueva imagen cargada por el usuario.
+        });
+    lector.readAsDataURL(archivo); //Transforma la lectura del archivo en un data URL.
+  });
+  
 });
+
   //Validacion de los campos del perfil
 (function () {
   "use strict";
@@ -43,6 +62,7 @@ document.addEventListener("DOMContentLoaded", () => {
           const apellido1 = document.getElementById("apellido1").value;
           const apellido2 = document.getElementById("apellido2").value;
           const numero = document.getElementById("numero").value;
+          
           //Creamos un objeto que contenga los datos del usuario
           datosUsuario = {
             nombre1: nombre1,
@@ -51,14 +71,18 @@ document.addEventListener("DOMContentLoaded", () => {
             apellido2: apellido2,
             email: sessionStorage.getItem("usuario"),
             numero: numero,
-            imagenPerfil: localStorage.getItem("imagenPerfil"),
+            imagenPerfil: imagenDePerfil.getAttribute('src'),
           };
+          
           console.log(datosUsuario)
           guardarCambios(datosUsuario);
+          
+
         }
         form.classList.add("was-validated");
       },
       false);
   });
+  
 })();
 
