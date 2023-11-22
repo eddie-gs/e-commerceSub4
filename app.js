ab5eccb72554f5cba290dae16043c6a2fea80fa1
Javier Gularte <133526577+eddie-gs@users.noreply.github.com>
2023-11-22 20:21:52 -0300

    commit #2

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const mariadb = require("mariadb");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 const SECRET_KEY = "Guiso de Pollo";
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
+app.use(express.json());
 app.use(express.static("JSON"));
+
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
